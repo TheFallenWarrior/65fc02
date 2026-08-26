f8392a0635cc1860b236015cd80815550e446a6b
TheFallenWarrior <thefallenwarrior666@gmail.com>
2026-08-26 08:40:15 -0300

    Normalize cmp argument in alu_sbc to boolean before using it

--- src/alu.c
+++ src/alu.c
@@ -29,7 +29,7 @@ uint8_t alu_adc(uint8_t a, uint8_t b){
 
 uint8_t alu_sbc(uint8_t a, uint8_t b, uint8_t cmp){
 	int16_t r;
-	uint8_t c = (mcs6500.p&PFLAG_C) | cmp;
+	uint8_t c = (mcs6500.p&PFLAG_C) | !!cmp;
 
 	r = a - b - (1-c);
 
